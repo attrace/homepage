2d291187724312842b6d6c8ce3469b0e23d8a956
unknown <dash0406dash@mail.ru>
2022-05-18 08:42:16 -0400

    update button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -5,6 +5,7 @@ import styles from "./styles.module.css";
 
 interface Props extends HTMLProps<HTMLButtonElement> {
   variant?: "secondary" | "primary" | "tertiary";
+  sizes?: "large" | "medium" | "small";
   className?: string;
   children?: ReactNode;
   style?: CSSProperties;
@@ -14,6 +15,7 @@ const Button: FC<Props> = ({
   children,
   className = "",
   variant,
+  sizes,
   onClick,
   style,
   type,
@@ -22,6 +24,8 @@ const Button: FC<Props> = ({
   const btnClass = clsx(styles.primary, {
     [styles.secondary]: variant === "secondary",
     [styles.tertiary]: variant === "tertiary",
+    [styles.medium]: sizes === "medium",
+    [styles.small]: sizes === "small",
     className,
   });
 
